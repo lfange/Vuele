596835ce4e6a6dfb2478f0e6ea3200ef299513ab
FanGe <653398363@qq.com>
2019-06-14 22:13:25 +0800

    1

--- Front/transcoding.vue
+++ Front/transcoding.vue
@@ -54,7 +54,7 @@ export default {
                         this.$emit('complete', text, this.value, this.scope);
                         this.displayText = text || this.emptyText;
                     })
-                    .catch(e => {  // second   // second
+                    .catch(e => {  // second  del second
                         // first
                         this.loading = false;
                         this.$emit('error', this.value, this.scope);
