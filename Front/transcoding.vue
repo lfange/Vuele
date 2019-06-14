be02ce168ca0f96cfbfce2e94ffd41803e5119d8
a653398363 <30554223+a653398363@users.noreply.github.com>
2019-06-14 20:46:51 +0800

    Update transcoding.vue

--- Front/transcoding.vue
+++ Front/transcoding.vue
@@ -54,7 +54,7 @@ export default {
                         this.$emit('complete', text, this.value, this.scope);
                         this.displayText = text || this.emptyText;
                     })
-                    .catch(e => {
+                    .catch((e) => {
                         this.loading = false
                         this.$emit('error', this.value, this.scope);
                         this.displayText = this.errorText || this.value;
